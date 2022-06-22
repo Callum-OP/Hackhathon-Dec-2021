76027db6d93c9993de030152d0c9382c555337db
Callum O'Prey <OPrey-C8@ulster.ac.uk>
2022-06-22 20:12:18 +0100

    Final version of web app

--- src/App.js
+++ src/App.js
@@ -1,28 +1,48 @@
-import './App.css';
+import './index.css';
 import React, {useState} from 'react'
+import Attend from './Attend'
 
 function App() {
 
   const initialTalks = []
-  const entry = ""
+
+  const [month, setMonth] = useState("")
+  const [week, setWeek] = useState("")
+
   const [currentCount, setCount] = useState(0)
   const [title, setTitle] = useState("")
   const [name, setName] = useState("")
   const [time, setTime] = useState("")
   const [date, setDate] = useState("")
   const [talks, setTalks]  = React.useState(initialTalks);
-  const listTalks = talks.map((talk) => (
-      <div className="talks" key={talk}>
-          <div className="talk">
-              {talk}
-          </div>
-      </div>
-  ))
+  let listTalks = ''
+  if (month != "") {listTalks = talks.filter(talk => (new Date(talk.date).getMonth() + 1).toString() === month).map((talk) => (
+    <div className="talks" key={talk.title}>
+        <div className="talk">
+            {talk.title} taken by {talk.name} at {talk.time} on {talk.date}
+            <Attend></Attend>
+            <br></br>
+        </div>
+    </div>
+  ))} else {
+    talks.map((talk) => (
+    <div className="talks" key={talk.title}>
+        <div className="talk">
+            <p>{talk.title} taken by {talk.name} at {talk.time} on {talk.date}</p>
+            <Attend></Attend>
+        </div>
+    </div>
+  ))}
 
   function handleAdd(event) {
       event.preventDefault();
-      const newEntry = entry.concat(title, " taken by ", name, " at ", time, " on ", date)
-      const list = talks.concat(newEntry)
+      let talk = {
+        title: title,
+        name: name,
+        time: time,
+        date: date
+      }
+      const list = talks.concat(talk)
       setTalks(list)
       setCount(currentCount + 1)
       setTitle("")
@@ -38,31 +58,47 @@ function App() {
       setName(event.target.value)
   }
   function handleDate(event) {
+      console.log(event)
       setDate(event.target.value)
   }
   function handleTime(event) {
     setTime(event.target.value)
   }
+  function handleMonth(event) {
+    setMonth(event.target.value)
+  }
+  function handleWeek(event) {
+    setWeek(event.target.value)
+  }
 
   return (
     <div className='techTalk'>
-      <h4>Add a Tech Talk</h4>
-        Title:
-        <input type='text' value={title} onChange={handleTitle}></input>
-        <br></br>
-        Name:
-        <input type='text' value={name} onChange={handleName}></input>
-        <br></br>
-        Time:
-        <input type='time' value={time} onChange={handleTime}></input>
-        <br></br>
-        Date:
-        <input type='date' value={date} onChange={handleDate}></input>
-        <br></br>
-        <button onClick={handleAdd}>Add</button>
-        <div>
+      <div className='add'>
+        <h4>Add a Tech Talk</h4>
+          Title:
+          <input type='text' value={title} onChange={handleTitle}></input>
+          <br></br>
+          Name:
+          <input type='text' value={name} onChange={handleName}></input>
+          <br></br>
+          Time:
+          <input type='time' value={time} onChange={handleTime}></input>
+          <br></br>
+          Date:
+          <input type='date' value={date} onChange={handleDate}></input>
+          <br></br>
+          <button onClick={handleAdd}>Add</button>
+      </div>
+        <div className='view'>
           <h4>View Tech Talks</h4>
-          <p>Number of tech talks available: {currentCount}</p>
+          Number of tech talks available: {currentCount}
+          <br></br>
+          <br></br>
+          View by Week:<input type='text' value={week} onChange={handleWeek}></input>
+          <br></br>
+          View by Month<input type='text' value={month} onChange={handleMonth}></input>
+          <br></br>
+          <br></br>
           {listTalks}
         </div>
     </div>
--- src/Attend.js
+++ src/Attend.js
@@ -6,11 +6,11 @@ function Attend() {
   const entry = ""
   const [currentCount, setCount] = useState(0)
   const [name, setName] = useState("")
-  const [attendees, setAttendee]  = React.useState(initialAttendees);
-  const listTalks = talks.map((talk) => (
-      <div className="talks" key={talk}>
-          <div className="talk">
-              {talk}
+  const [attendees, setAttendees]  = React.useState(initialAttendees);
+  const listNames = attendees.map((name) => (
+      <div className="name" key={name}>
+          <div className="name">
+              {name} is attending
           </div>
       </div>
   ))
@@ -18,48 +18,28 @@ function Attend() {
   function handleAdd(event) {
       event.preventDefault();
       const newEntry = entry.concat(name)
-      const list = talks.concat(newEntry)
+      const list = attendees.concat(newEntry)
       setAttendees(list)
       setCount(currentCount + 1)
       setName("")
   }
 
-  function handleTitle(event) {
-      setTitle(event.target.value)
-  }
   function handleName(event) {
       setName(event.target.value)
   }
-  function handleDate(event) {
-      setDate(event.target.value)
-  }
-  function handleTime(event) {
-    setTime(event.target.value)
-  }
 
   return (
-    <div className='techTalk'>
-      <h4>Add a Tech Talk</h4>
-        Title:
-        <input type='text' value={title} onChange={handleTitle}></input>
-        <br></br>
-        Name:
+    <div className='attendees'>
+        Enter Name:
         <input type='text' value={name} onChange={handleName}></input>
         <br></br>
-        Time:
-        <input type='time' value={time} onChange={handleTime}></input>
-        <br></br>
-        Date:
-        <input type='date' value={date} onChange={handleDate}></input>
-        <br></br>
-        <button onClick={handleAdd}>Add</button>
+        <button onClick={handleAdd}>Attend</button>
         <div>
-          <h4>View Tech Talks</h4>
-          <p>Number of tech talks available: {currentCount}</p>
-          {listTalks}
+          Number of attendees: {currentCount}
+          {listNames}
         </div>
     </div>
   )
 }
 
-export default App;
+export default Attend;
